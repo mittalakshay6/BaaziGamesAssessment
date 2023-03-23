a5b7118f78dd918195bdf889669aaddc7b2d4144
Akshay Mittal <akshay.m@turvo.com>
2023-03-23 22:33:46 +0530

    StackUsingQueues :: Add main method and class

--- StackUsingQueues/src/main/java/org/baazigames/Main.java
+++ StackUsingQueues/src/main/java/org/baazigames/Main.java
@@ -1,7 +1,19 @@
 package org.baazigames;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        Stack<Integer> sink = new Stack<>();
+        ProducerThread<IntegerProducer> producerThread = new ProducerThread<>(
+                5,
+                new IntegerProducer(sink, 50)
+        );
+        ConsumerThread<IntegerConsumer> consumerThread = new ConsumerThread<>(
+                5,
+                new IntegerConsumer(sink)
+        );
+        producerThread.start();
+        consumerThread.start();
+        producerThread.join();
+        consumerThread.join();
     }
 }
